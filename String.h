a979e3c91eaf19dff765918213714b10aa036503
Hegehog <workjob2077@gmail.com>
2025-01-02 17:12:41 +0300

    Creat move constructor

--- String.h
+++ String.h
@@ -11,19 +11,24 @@ private:
 public:
     static int count;
 
+    //Конструкторы
+
     String() : str{ new char[80] }, size{ 80 } { count++; }
     String(const char* strUser);
     String(int sizeUser) :str{ new char[sizeUser] }, size{ sizeUser } { count++; };
     String(int sizeUser, const char* strUser);
 
-    
+    //Конструктор копирования 
     String(String& User);
+    //Конструктор переноса
     String(String&& obj)noexcept;
 
+    //Акссесоры
     void input();
     void output() const;
 
     static int getCount() { return count; }
 
+    //Деструктор
     ~String();
 };
